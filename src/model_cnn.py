3a3f799a7f54ee4ba6c17a46fcd1fa9f10b0c45f
Pouya Mirzaeizadeh <104144393+pouyamirzaiy@users.noreply.github.com>
2026-05-20 00:08:29 +0330

    Update model_cnn.py

--- src/model_cnn.py
+++ src/model_cnn.py
@@ -1,5 +1,5 @@
 """
-Auto-extracted CNN model code from notebook on 2025-09-07T22:52:35.169848
+Auto-extracted CNN model code from notebook
 """
 
 # model.py
@@ -9,5 +9,5 @@ from keras.models import load_model
 def load_age_model():
     
     # Load the trained model
-    model = load_model('saved_model.h5')  # Adjust the filename based on your saved model
+    model = load_model('saved_model.h5')  
     return model
